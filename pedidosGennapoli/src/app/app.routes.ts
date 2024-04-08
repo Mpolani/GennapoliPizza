c27134ee74d91fa57699f979f5df87de2d905328
fpolania <lassofabian36@gmail.com>
2024-04-08 15:01:22 -0500

    Se crea componente login

--- pedidosGennapoli/src/app/app.routes.ts
+++ pedidosGennapoli/src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginAndRegisterComponent } from './login-and-register/login-and-register.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'loginOrRegister', component: LoginAndRegisterComponent }
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
